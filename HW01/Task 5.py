064a5e70f9872c7a1899508bc97b083789d98052
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-12-15 12:56:55 +0600

    task 5

---
+++ HW01/Task 5.py
@@ -0,0 +1,13 @@
+# Напишите программу, которая по заданному номеру четверти,
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+quarter = int(input('Введите номер четверти '))
+if quarter==1:
+    print('X>0 and Y>0')
+elif quarter==2:
+    print('X<0 and Y>0')
+elif quarter==3:
+    print('X<0 and Y<0')
+elif quarter==4:
+    print('X>0 and Y<0')
+else:
+    print('Ошибка! введите другие данные')
