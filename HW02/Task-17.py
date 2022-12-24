42c4302d0af82ae98d0cedc579c3533c5880f51d
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-12-24 13:42:55 +0600

    task-4 другие способы

--- HW02/Task-17.py
+++ HW02/Task-17.py
@@ -2,16 +2,31 @@
 # Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в отдельном списке( пример n=4, lst1=[4,-2,1,3] -
 #  списко на основе n, а позиции элементов lst2=[3,1].
-n=int(input('введите число '))
-lst2=[3,1]
-lst1=[]
-mult = 1
-for i in range(1,n+1):
-    from random import randint
-    element = randint(-n,n)
-    lst1.append(element)
+# n=int(input('введите число '))
+# lst2=[3,1]
+# lst1=[]
+# mult = 1
+# for i in range(1,n+1):
+#     from random import randint
+#     element = randint(-n,n)
+#     lst1.append(element)                   # lst1.append(random.randint(-n,n))
+# print(lst1)
+# for e in range(len(lst2)):
+#     a = lst2[e]
+#     mult = mult * lst1[a]
+# print(mult)
+
+import random
+
+n = int(input("Введите N: "))
+
+lst1 = list(random.randint(-n, n) for i in range(n))
 print(lst1)
-for e in range(len(lst2)):
-    a = lst2[e]
-    mult = mult * lst1[a]
-print(mult)
+
+lst2 = list(map(int, input("Введите позиции чисел: ").split(", ")))
+
+result = 1
+for i in lst2:
+    result *= lst1[i]
+
+print(result)
