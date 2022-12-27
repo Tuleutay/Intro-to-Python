071e6eed1d77ec4c4092bcea88b8b36332bd3451
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-12-27 17:50:46 +0600

    task-2 hw-4 clean

---
+++ HW04/Task-3.py
@@ -0,0 +1,15 @@
+# Задайте последовательность чисел. Напишите программу, которая выведет
+# список неповторяющихся элементов исходной последовательности.
+nums = list(map(int, input("Введите числа через пробел: ").split(" ")))
+def list_of_non_repeating_elements(list):
+    i=0
+    while i < len(list):
+        x = list[i]
+        if list.count(x)>1:
+            while list.count(x)>0:
+                list.remove(x)
+        else:
+            i+=1
+    return list
+result = list_of_non_repeating_elements(nums)
+print(result)
