3717f80e58dbde1dd17ff986a0b0224b2d42965c
Tuleutay <tuleutay.dairbekov@gmail.com>
2023-01-05 11:55:41 +0600

    task-4 hw-4 done commit

--- HW04/task-4.py
+++ HW04/task-4.py
@@ -4,22 +4,27 @@
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 import random
-k=int(input('Введите натуральную степень '))+1
-# lst1 = list(random.randint(0, 100) for i in range(0,k))
-# print(lst1)
-# for i in range(k):
-#     print(f'{lst1[i]}x**{i}', end=" ")
+
+k = int(input('Введите натуральную степень ')) + 1
+
 def get_odds(num):
-    x=None
+    x = None
     lst1 = list(random.randint(0, 100) for i in range(num))
-    print(lst1)
-    lst2 = []
-    i=1
-    while i < len(lst1):
-        # lst2.append(lst1[i]*x**i)
-        print(f'{lst1[i]}*x**{i}')
-        i+=1
-    # lst2[0]=lst1[0]
-    return i
+    stringa = ''
+    # while i < len(lst1):
+    for i in range(len(lst1) - 1, -1, -1):
+        if i == 1:
+            stringa += f'{lst1[i]}*x + '
+        elif i == 0:
+            stringa += str(lst1[0])
+        else:
+            stringa += f'{lst1[i]}*x**{i} + '
+    return stringa
+
 result = get_odds(k)
 print(result)
+
+with open("result.txt", 'w') as test_file:
+    test_file.write(result)
+
+
