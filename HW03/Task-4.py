ad567086cb145396b90a6e843abeec30ebf9c2a6
Tuleutay <tuleutay.dairbekov@gmail.com>
2023-01-19 01:48:01 +0600

    sem 5

--- HW03/Task-4.py
+++ HW03/Task-4.py
@@ -4,8 +4,23 @@
 # - 3 -> 11
 # - 2 -> 10
 n = int(input('введите число '))
-b = ''
-while n > 0:
-    b = str(n % 2) + b
-    n = n // 2
-print(b)
+# b = ''
+# while n > 0:
+#     b = str(n % 2) + b
+#     n = n // 2
+# print(b)
+
+# VTOROY SPOSOB
+
+list = []
+while n > 1:
+    list.insert(0, n % 2)
+    n = int(n / 2)
+
+list.insert(0, n)
+
+s = ""
+for i in list:
+    s += str(i)
+
+print(s)
